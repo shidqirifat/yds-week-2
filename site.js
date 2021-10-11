ae3a91aab08461b8370c1f5d363871532444df78
shidqirifat <rifatpangestu87@gmail.com>
2021-10-11 11:17:12 +0700

    perbaiki file index: tambah elemen untuk btn-top dan btn-mobile-menu; style: buat style untuk btn-top dan btn-mobile-menu; buat file js: buat program untuk menampilkan menu mobile dan scroll top

--- site.js
+++ site.js
@@ -1,1 +1,21 @@
 // Kode js untuk file index.html
+const btn_menu = document.getElementById('btn-menu');
+const nav_mobile = document.getElementById('nav-mobile');
+const btn_top = document.getElementById('btn-top');
+
+btn_menu.addEventListener("click", () => {
+    nav_mobile.classList.toggle("toggled-off");
+})
+
+const lengthPage = document.documentElement.scrollHeight;
+
+window.onscroll = () => {
+    if (document.documentElement.scrollTop > lengthPage * 0.25) {
+        btn_top.classList.remove("nav-top-disable");
+    }
+    else btn_top.classList.add("nav-top-disable");
+}
+
+btn_top.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+})
